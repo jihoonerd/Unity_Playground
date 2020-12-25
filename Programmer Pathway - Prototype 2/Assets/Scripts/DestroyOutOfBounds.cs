bb56bd71b557593f58dfe6869d4f46e3a153cd89
Jihoon Kim <jihoonerd@outlook.com>
2020-12-25 16:27:59 +0900

    Add collision effect

--- Programmer Pathway - Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Programmer Pathway - Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -9,7 +9,7 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,8 +18,10 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z > upperBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound)
+        }
+        else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
--- Programmer Pathway - Prototype 2/Assets/Scripts/PlayerController.cs
+++ Programmer Pathway - Prototype 2/Assets/Scripts/PlayerController.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 10.0f;
+    public float speed = 15.0f;
+    public float xRange = 20.0f;
     public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -xRange){
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-        if (transform.position.x > xRange){
-            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
