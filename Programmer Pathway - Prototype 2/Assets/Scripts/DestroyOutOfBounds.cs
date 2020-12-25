40b37939fed77ee978981e8578114112485c72dd
Jihoon Kim <jihoonerd@outlook.com>
2020-12-25 14:51:51 +0900

    Add projectile and removal of game objects

---
+++ Programmer Pathway - Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float upperBound;
+    public float lowerBound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > upperBound)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Programmer Pathway - Prototype 2/Assets/Scripts/PlayerController.cs
+++ Programmer Pathway - Prototype 2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +22,11 @@ public class PlayerController : MonoBehaviour
         }
         if (transform.position.x > xRange){
             transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
