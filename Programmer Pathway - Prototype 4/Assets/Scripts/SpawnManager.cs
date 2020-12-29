34920b3b6f21ab20c4644d462cb46aa3796ffcbe
Jihoon Kim <jihoonerd@outlook.com>
2020-12-29 17:40:03 +0900

    Add powerup indicator

--- Programmer Pathway - Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Programmer Pathway - Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerUpPrefab;
+
     private float spawnRange = 9;
     // Start is called before the first frame update
     void Start()
