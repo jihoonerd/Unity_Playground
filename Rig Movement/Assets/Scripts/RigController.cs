fec44e6c8845507b63e9d672210fdbf113760b7c
Jihoon Kim <jihoonerd@outlook.com>
2020-12-30 12:23:51 +0900

    Prototype 4 Challenge

---
+++ Rig Movement/Assets/Scripts/RigController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigController : MonoBehaviour
+{
+    private GameObject elbowL;
+    private Rigidbody elbowLRigidbody;
+    private GameObject handL;
+    // Start is called before the first frame update
+    void Start()
+    {
+        elbowL = GameObject.Find("elbowL");
+        elbowLRigidbody = elbowL.GetComponent<Rigidbody>();
+        elbowLRigidbody.AddForce(Vector3.right * 0.1f, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
