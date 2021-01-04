73b94b049b928726475d6970b310ffe0a7656a15
Jihoon Kim <jihoonerd@outlook.com>
2021-01-04 15:31:10 +0900

    Ternary operator

---
+++ Programming Systems and Architecture/Assets/Scripts/Properties/TernaryOperator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TernaryOperator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int health = 10;
+        string message;
+
+        message = health > 0 ? "Player is alive" : "Player is dead";
+        Debug.Log(message);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
