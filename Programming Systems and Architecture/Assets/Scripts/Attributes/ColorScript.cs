e0fd085df2e07803c52e213a9fd79e9c98b219ac
Jihoon Kim <jihoonerd@outlook.com>
2021-01-14 18:51:22 +0900

    Add: attributes

---
+++ Programming Systems and Architecture/Assets/Scripts/Attributes/ColorScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ColorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().sharedMaterial.color = Color.blue;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Programming Systems and Architecture/Assets/Scripts/Attributes/SpinScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinScript : MonoBehaviour
+{
+    [Range(-100, 100)] public int speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
+    }
+}
