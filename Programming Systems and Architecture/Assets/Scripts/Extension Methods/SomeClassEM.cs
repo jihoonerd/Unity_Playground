5a43644743073b6a19edebcf622766b6412ce00b
Jihoon Kim <jihoonerd@outlook.com>
2021-01-14 17:52:19 +0900

    Add: extension methods

---
+++ Programming Systems and Architecture/Assets/Scripts/Extension Methods/SomeClassEM.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeClassEM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This is useful when you don't have access to a class, but needs additional functionality from it.
+        transform.ResetTransformation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
