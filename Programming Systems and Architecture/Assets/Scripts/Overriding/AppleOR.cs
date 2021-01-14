eb8093f55b4ea6d8fcc4ce071465e17ccbebd44c
Jihoon Kim <jihoonerd@outlook.com>
2021-01-14 17:36:48 +0900

    Add: overriding

---
+++ Programming Systems and Architecture/Assets/Scripts/Overriding/AppleOR.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleOR : FruitOR
+{
+    public AppleOR()
+    {
+        Debug.Log("1st Apple Constructor Called");
+    }
+
+    //These methods overrides and therefore can override any virtual methods in the parent class.
+    public override void Chop()
+    {
+        base.Chop();
+        Debug.Log("The apple has been chopped.");
+    }
+
+    public override void SayHello()
+    {
+        base.SayHello();
+        Debug.Log("Hello, I am an apple.");
+    }
+}
---
+++ Programming Systems and Architecture/Assets/Scripts/Overriding/FruitSaladOR.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSaladOR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AppleOR myApple = new AppleOR();
+        myApple.SayHello();
+        myApple.Chop();
+
+        FruitOR myFruit = new AppleOR();
+        myFruit.SayHello();
+        myFruit.Chop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
